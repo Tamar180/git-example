05c00f2e94e2ca66d19025bf2c44601ca65caa97
ZipiLindenfeld <z0556776310@gmail.com>
2024-07-29 11:42:32 +0300

    change

--- git example 3/git example 3.cpp
+++ git example 3/git example 3.cpp
@@ -8,10 +8,10 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "good morning Zipi!\n";
     std::cout << "good morning Sari!\n";
-    std::cout << "good morning Tamar!\n";
-    std::cout << "good morning everyone!\n";
+    std::cout << "goood morning everyone!\n";
+    std::cout << "god morning Tamar!\n";
     std::cout << "good afternoon everyone!\n";
-    std::cout << "good afternoon sari!\n";
+    std::cout << "good afternoon zipi!\n";
 
 }
 
